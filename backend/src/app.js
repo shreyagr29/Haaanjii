9c7a3eef0dade39980546901f4c4e671633f64a2
Shrey Agrawal <shreyvinayakagrawal1111@gmail.com>
2025-04-13 00:27:57 +0530

    Backend change

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,16 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 5000);
-app.use(cors());
+app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true
+  }));
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
 
